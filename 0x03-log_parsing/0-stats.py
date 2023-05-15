bed65f58eb1ef641f2d9502d0142f2ada41aa371
Faith Biketi <78912860+FaithB01@users.noreply.github.com>
2023-05-15 14:11:32 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
-
+'''This module recieved logs from command line,
+parse and print out some stats.'''
 import sys
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+def match_format(line=""):
+    '''Check if the line match required format using regex'''
+    try:
+        line_spt = line.split()
+        first_ocatet = int(line_spt[0].split('.')[0])
+        # format is valid?
+        if len(line_spt) != 9 or first_ocatet > 255 or first_ocatet < 0:
+            return 0
+        s_code = int(line_spt[-2])
+        if s_code not in list(codes.keys()):
+            return 0
+    except:
+        return 0
+    return 1
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+
+def log_line(line):
+    '''Take line and take necessary data for statistics.'''
+    try:
+        line = line[:-1]
+        words = line.split(" ")
+        file_size[0] += int(words[-1])
+        code = int(words[-2])
+        if code in codes:
+            codes[code] += 1
+    except:
+        pass
+
+
+def print_stats():
+    '''print statistics of current files'''
+    print("File size: {}".format(file_size[0]))
+    for code in sorted(codes.keys()):
+        if codes[code]:
+            print("{}: {}".format(code, codes[code]))
+
+
+if __name__ == "__main__":
+    file_size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    count = 1
 
     try:
         for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
+            # Skip, if the line format is not valid
+            if not match_format(line):
+                continue
+            # log data for statistics
+            log_line(line)
             if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
+                print_stats()
+            count += 1
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
+        print_stats()
         raise
+    print_stats()
