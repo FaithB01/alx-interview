451ab2e28c6e7206fc9912fe38bbc39b766e7608
Faith Biketi <78912860+FaithB01@users.noreply.github.com>
2023-05-23 12:59:44 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,77 +1,54 @@
 #!/usr/bin/python3
-'''The N queens puzzle is the challenge of placing N non-attacking queens
-on an N×N chessboard. '''
+"""
+N queens
+"""
+
 import sys
-'''
-#1 shitart from the leftmost column
-#2 if all queens are placed, return.
-#3 try all rows in the current column
-# do following for every tried row.
-# a. check if the queen can be placed safely
-# and mark the cell, then check if this can lead to a solution
-# b. if yes, return true
-#c. else, unmarked the cell and goto #a.
-# if all rows have been tried and nothing worked, return false
-# to triggerd backtracking
 
-# [0,1]
-
-'''
-if len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
 
 try:
-    num = int(sys.argv[1])
+    n_q = int(sys.argv[1])
 except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    print('N must be a number')
+    exit(1)
 
-if num < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-
-def solveNQueens(n):
-    """
-    Places N non-attacking queens on an NxN chessboard.
-    """
-    col, pos, neg = set(), set(), set()
-    current_board = [[] for n in range(n)]
-    solved_board = []
-
-    def backtrack(row):
-        """
-        Tool for solving constraint satisfaction problems.
-        """
-        if row == n:
-            copy = current_board.copy()
-            solved_board.append(copy)
-            return
-
-        for c in range(n):
-            if c in col or (row + c) in pos or (row - c) in neg:
-                continue
-
-            col.add(c)
-            pos.add(row + c)
-            neg.add(row - c)
-
-            current_board[row] = [row, c]
-
-            backtrack(row + 1)
-
-            col.remove(c)
-            pos.remove(row + c)
-            neg.remove(row - c)
-            current_board[row] = []
-
-    backtrack(0)
-
-    return solved_board
+if n_q < 4:
+    print('N must be at least 4')
+    exit(1)
 
 
-if __name__ == "__main__":
-    boards = solveNQueens(num)
-    for board in boards:
-       print(board)
+def solve_nqueens(n):
+    '''self descriptive'''
+    if n == 0:
+        return [[]]
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
+
+
+def attack_queen(square, queen):
+    '''self descriptive'''
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
+
+
+def safe_queen(sqr, queens):
+    '''self descriptive'''
+    for queen in queens:
+        if attack_queen(sqr, queen):
+            return False
+    return True
+
+
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
